43791cb59f95e5a652b08e6d769ce4cd53440a73
dave397 <ologunlekodavid@gmail.com>
2023-06-20 08:43:37 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - turns number to absolute
+ * @n: The integer accepted
+ * Return: absolute value
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return n * -1;
+}
