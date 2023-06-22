dfd9aeb415d103cdc2cd281b1fdc82334e01dd12
dave397 <ologunlekodavid@gmail.com>
2023-06-22 12:04:52 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * more_numbers - Prints number 1-14 ten times
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int count;
+
+	int c_two;
+
+	for (count = 0; count < 10; count++)
+	{
+		for (c_two = 0; c_two <= 14; c_two++)
+		{
+			if (c_two < 10)
+			{
+				_putchar(c_two % 10 + '0');
+			}
+			else
+			{
+				_putchar(c_two / 10 + '0');
+				_putchar(c_two % 10 + '0');
+			}
+		}
+		c_two = 0;
+		_putchar('\n');
+	}
+}
