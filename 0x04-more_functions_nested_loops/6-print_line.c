2db4147166aae2fc661a7f9cf11cd486a22602b7
dave397 <ologunlekodavid@gmail.com>
2023-06-22 12:44:59 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_line - Prints line
+ * @n: the number of times line is printed
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int count;
+
+	if (n < 1)
+		_putchar('\n');
+	else
+	{
+		for (count = 0; count <= n; count++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
