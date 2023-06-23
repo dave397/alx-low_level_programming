8a59cb9beffb8a699bcea52a46ab14f9fb8d1323
dave397 <ologunlekodavid@gmail.com>
2023-06-23 15:17:22 -0700

    print number

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_number - print the numbers inputed
+ * @n: The character inputed
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	int cont = n;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		cont = -cont;
+	}
+	if (cont / 10)
+		print_number(cont / 10);
+	_putchar(cont % 10 + '0');
+}
