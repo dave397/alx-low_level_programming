352cd0d7ebb699d5e895cbba3c9b85ea868861ff
dave397 <ologunlekodavid@gmail.com>
2023-06-26 18:34:10 -0700

    nice

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,13 +9,14 @@
 
 int _atoi(char *s)
 {
-	int count, i, h, digit, sign;
+	int count, i, h, digit, sign, entered;
 
 	count = 0;
 	i = 0;
 	h = 1;
 	digit = 1;
 	sign = 1;
+	entered = 0;
 
 	while (s[count])
 		count++;
@@ -30,6 +31,7 @@ int _atoi(char *s)
 			break;
 		if (s[i] >= '0' && s[i] <= '9')
 		{
+			entered = 1;
 			if (h == 1)
 			{
 				digit = s[i] - '0';
@@ -40,5 +42,7 @@ int _atoi(char *s)
 		}
 		i++;
 	}
-	return (digit * sign);
+	if (entered)
+		return (digit * sign);
+	return (0);
 }
