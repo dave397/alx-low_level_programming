5afa6eb3ad078e5e4e1cc6e91f578e4357a10d0e
dave397 <ologunlekodavid@gmail.com>
2023-06-26 19:33:32 -0700

    last 5

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -11,7 +11,7 @@
 int main(void)
 {
 	int pass[100];
-	int i, sum;
+	int i, sum, n;
 
 	sum = 0;	
 
@@ -22,6 +22,13 @@ int main(void)
 		pass[i] = rand() % 78;
 		sum += (pass[i] + '0');
 		putchar(pass[i] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
+		}
 	}
 
 	return (0);
