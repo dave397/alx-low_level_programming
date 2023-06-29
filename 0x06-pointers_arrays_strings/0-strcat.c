a3b5bb4c2ed4422eba4269574558a5dab0e26d01
dave397 <ologunlekodavid@gmail.com>
2023-06-29 11:38:15 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenate string
+ * @dest: first string
+ * @src: second string
+ */
+
+
+char *_strcat(char *dest, char *src)
+{
+	int count = 0;
+
+	int i = 0;
+
+	while (dest[i])
+	{
+		if (dest[i] == '\n')
+			break;
+		i++;
+	}
+
+	while (src[count])
+	{
+		dest[i + count] = src[count];
+		count++;
+	}
+
+	src[count] = '\0';
+
+	return (dest);
+}
