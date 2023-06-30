68a813921af2014dc9d9a16b776a5d1419dd7087
dave397 <ologunlekodavid@gmail.com>
2023-06-30 11:04:45 -0700

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalize first character of string
+ * @n: string to convert
+ * Return: converted string
+ */
+
+char *cap_string(char *n)
+{
+	int c = 0;
+
+	while (n[c])
+	{
+		if (n[c] == '\0')
+			break;
+		if (
+		(c > 0 && n[c - 1] == ' ') ||
+		(c > 0 && n[c - 1] == '\n') ||
+		(c > 0 && n[c - 1] == '.')
+		)
+		{
+			if (n[c] >= 'a' && n[c] <= 'z')
+				n[c] = n[c] - 32;
+		}
+		c++;
+	}
+	return (n);
+}
