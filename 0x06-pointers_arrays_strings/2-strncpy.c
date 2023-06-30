512ed6a9329d344e951271503aa6e7a281925f6d
dave397 <ologunlekodavid@gmail.com>
2023-06-29 17:08:31 -0700

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strncpy - copy string
+ * @dest: first string
+ * @src: second string
+ * @n: integer
+ * Return: character
+ */
+
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int count, i;
+
+	i = 0;
+	count = 0;
+
+	while (src[i])
+	{
+		if (src[i] == '\n')
+			break;
+		i++;
+	}
+
+	while (count < n && src[count])
+	{
+		dest[count] = src[count];
+		count++;
+	}
+	return (dest);
+}
