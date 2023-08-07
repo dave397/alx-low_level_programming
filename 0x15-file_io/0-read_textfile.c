bcdbe77ceb2c749d1b5ba22305a21e928d39524b
david397 <ologunlekodavid@gmail.com>
2023-08-07 23:31:12 +0100

    format code

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,48 +1,46 @@
+#include "main.h"
 #include <fcntl.h>
 #include <unistd.h>
-#include "main.h"
 
 /**
  * read_textfile -read the content of text file
- * @filename: name of file 
+ * @filename: name of file
  * @letters: length to read
  * Return: letter length, otherwise 0 (fail)
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    ssize_t read_bytes;
-    
-    int reader = open(filename, O_RDONLY);
-    
-    char buffer[1024];
+	ssize_t read_bytes;
 
-    size_t total = 0;
+	int reader = open(filename, O_RDONLY);
 
-    if (filename == NULL)
-        return (0);
+	char buffer[1024];
 
+	size_t total = 0;
 
+	if (filename == NULL)
+		return (0);
 
-    if (reader == -1)
-        return (0);
-    
-    while ((read_bytes = read(reader, buffer, sizeof(buffer))) > 0)
-    {
-        ssize_t written = write(STDOUT_FILENO, buffer, read_bytes);
+	if (reader == -1)
+		return (0);
 
-        if (read_bytes != written)
-        {
-            close(reader);
-            return (0);
-        }
+	while ((read_bytes = read(reader, buffer, sizeof(buffer))) > 0)
+	{
+		ssize_t written = write(STDOUT_FILENO, buffer, read_bytes);
 
-        total += written;
-        if (total >= letters)
-            break;
-    }
+		if (read_bytes != written)
+		{
+			close(reader);
+			return (0);
+		}
 
-    close(reader);
+		total += written;
+		if (total >= letters)
+			break;
+	}
 
-    return (total);
+	close(reader);
+
+	return (total);
 }
