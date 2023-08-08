d7179576209682c29f3c5433c994f54c1b9b0b70
david397 <ologunlekodavid@gmail.com>
2023-08-08 12:15:40 +0100

    added error function

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,8 +6,27 @@
 #define BUFFER 1024
 
 /**
+ * exit_with_error - exit code with error
+ * @code: error code
+ * @message: error message
+ * @...: additional arguments
+*/
+
+void exit_with_error(int code, char *message,  ...)
+{
+	va_list args;
+
+	va_start(args, message);
+
+	dprintf(2, message, args);
+
+	exit(code);
+}
+
+/**
  * main - check the code
- *
+ * @ac: number of passed parameter
+ * @av: pointer of passed parameter
  * Return: Always 0.
  */
 
@@ -18,16 +37,16 @@ int main(int ac, char **av)
 	int file_from, file_to, bytes_w, bytes_r;
 
 	if (ac != 3)
-		dprintf(97, "Usage: cp %s %s\n", av[1], av[2]);
+		exit_with_error(97, "Usage: cp %s %s\n", av[1], av[2]);
 
 	file_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	file_from = open(av[1], O_RDONLY);
 
 	if (file_to == -1)
-		dprintf(98, "Error: Can't write to %s\n", av[2]);
+		exit_with_error(98, "Error: Can't write to %s\n", av[2]);
 
 	if (file_from == -1)
-		dprintf(98, "Error: Can't read from file %s\n", av[1]);
+		exit_with_error(98, "Error: Can't read from file %s\n", av[1]);
 
 	while ((bytes_r = read(file_from, buffer, BUFFER)) > 0)
 	{
@@ -35,20 +54,22 @@ int main(int ac, char **av)
 
 		if (bytes_w == -1 || bytes_r != bytes_w)
 		{
-            close(file_to);
-            close(file_from);
-			dprintf(99, "Error:Can't write to %s\n", av[2]);
+			close(file_to);
+			close(file_from);
+			exit_with_error(99, "Error:Can't write to %s\n", av[2]);
 		}
 	}
 
 	if (bytes_r == -1)
-		dprintf(98, "Error: Can't read from file %s\n", av[1]);
+		exit_with_error(98, "Error: Can't read from file %s\n", av[1]);
 
 	if (close(file_from) == -1)
-		dprintf(100, "Error: Can't close fd %s\n", av[1]);
+		exit_with_error(100, "Error: Can't close fd %s\n", av[1]);
 
 	if (close(file_to) == -1)
-		dprintf(100, "Error: Can't close fd %s\n", av[2]);
+		exit_with_error(100, "Error: Can't close fd %s\n", av[2]);
 
 	return (0);
+
 }
+
