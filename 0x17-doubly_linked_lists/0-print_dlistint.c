6e81e90bce50db36d15500831efa5b737c1cb281
david397 <ologunlekodavid@gmail.com>
2023-12-08 12:11:36 +0100

    correct o

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,7 +10,7 @@ size_t print_dlistint(const dlistint_t *h)
 	int count = 0;
 	const dlistint_t *start = h;
 
-	if (h == NULL || h->next == NULL)
+	if (h == NULL)
 		return (0);
 	while (start != NULL)
 	{
